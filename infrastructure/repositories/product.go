9da46191cc1c45405c5aa82e5247632c2b57ad8e
rennanbadaro <rennanrb@icloud.com>
2021-02-17 01:07:40 -0300

    feat: implement product repository

---
+++ infrastructure/repositories/product.go
@@ -0,0 +1,34 @@
+package repositories
+
+import "github.com/rennanbadaro/discount-calculator/infrastructure/storage"
+
+type IProductRepository interface {
+	FetchByID(id string) (*Product, error)
+}
+
+type ProductRepository struct {
+	dbClient *storage.PostgresClient
+}
+
+type Product struct {
+	Id           string
+	PriceInCents int
+	Title        string
+	Description  string
+}
+
+func (pr *ProductRepository) FetchByID(id string) (*Product, error) {
+	query := "SELECT id, title, description, price_in_cents from products WHERE id=$1"
+	row := pr.dbClient.Conn.QueryRow(query, id)
+
+	p := &Product{}
+	if err := row.Scan(&p.Id, &p.Title, &p.Description, &p.PriceInCents); err != nil {
+		return nil, err
+	}
+
+	return p, nil
+}
+
+func NewProductRepository(dbClient *storage.PostgresClient) *ProductRepository {
+	return &ProductRepository{dbClient: dbClient}
+}
