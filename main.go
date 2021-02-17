05f178286dd345a921793ef111e38657d6119299
rennanbadaro <rennanrb@icloud.com>
2021-02-17 03:06:05 -0300

    feat(app): create main func

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/rennanbadaro/discount-calculator/cmd/grpc"
+)
 
 func main() {
-	fmt.Println("sup sup")
+	if os.Getenv("APP_ENV") == "development" {
+		if err := godotenv.Load(); err != nil {
+			log.Fatalf("failed to load .env file: %v", err)
+		}
+	}
+
+	if err := grpc.StartServer(); err != nil {
+		log.Fatalf("failed to start app: %v", err)
+	}
 }
